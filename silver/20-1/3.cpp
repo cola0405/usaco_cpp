fc5a807b5e1a774d4c8218d871ae386ad36f615e
Cola <1020151695@qq.com>
2023-12-10 13:10:03 +0800

    二分+并查集

--- silver/20-1/3.cpp
+++ silver/20-1/3.cpp
@@ -1,21 +1,30 @@
+// 二分upper_bound + 并查集(确认是否同一连通分量) -- 用dfs确认也可以
 #include <bits/stdc++.h>
 using namespace std;
 
 int N,M;
+vector<int> cows = {0};  // 把index化1
 vector<vector<int>> path;
 unordered_set<int> diff;
+vector<int> r;  // 并查集
+
+int find(int node){
+    if(node == r[node]) return node;
+    r[node] = find(r[node]);
+    return r[node];
+}
 
 int valid(int width){
-    unordered_set<int> group;
+    r.clear();
+    for(int i=0; i<=N; i++) r.push_back(i);
+
     for(auto it: path){
         int a=it[0], b=it[1], c=it[2];
-        if(c >= width){     // 看大于或等于当前width的通道能够消除哪些位置
-            group.insert(a);
-            group.insert(b);
-        }
+        if(c >= width) r[find(b)] = r[find(a)];  // 以width作为虫洞最低限度
     }
-    for(auto pos: diff){     
-        if(group.find(pos) == group.end()) return false;
+
+    for(auto pos: diff){
+        if(find(pos) != find(cows[pos])) return false;  // 如果pos和cows[pow]在同一连通分量，则可以通过虫洞排好序
     }
     return true;
 }
@@ -29,6 +38,7 @@ int main(){
         int cow;
         cin>>cow;
         if(cow != i+1) diff.insert(i+1);
+        cows.push_back(cow);
     }
     if(diff.size() == 0){
         cout<<-1<<endl;
@@ -47,7 +57,7 @@ int main(){
     // binary seach
     int low=0, high=wh.size()-1;
     while(low<high){
-        int mid = (low+high+1)/2;
+        int mid = (low+high+1)/2;   // upper bound
         if(valid(wh[mid])){
             low = mid;
         }
