8e2bfca122d713bd2322a264f02f6ff9fa83cbe7
cola <1020151695@qq.com>
2023-12-23 20:51:25 +0800

    23-2-1 ...!!!!!!!

--- silver/23-2/1_1.cpp
+++ silver/23-2/1_1.cpp
@@ -4,18 +4,24 @@
 using namespace std;
 
 int T,N;
-long long tC,tM;
+long long C0,M0;
 long long friends[100][3];
 
 bool valid(int money){
-    long long cLow = 0, cHigh = tC-1;     // 给 c upgrade 的可行范围
+    long long cLow = 0, cHigh = C0-1;     // 给 c upgrade 的可行范围
     for(int i=0; i<N; i++){
         long long needC=friends[i][0], needM=friends[i][1], wait=friends[i][2];
-        if(needC == needM) continue;
-        cHigh = (wait + needM*money - needM*tM - needC*tC) / (needM-needC);
-        cLow = ((tC+tM) + needM*money - needM*tM - needC*tC) / (needM-needC);
+        if(needC < needM){  // 不等式变换时，涉及到 (needM-needC) , 需要判断正负
+            cHigh = min(cHigh, static_cast<long long>(floor((wait + needM*(money-M0) - needC*C0) / float(needM-needC))));
+            cLow = max(cLow, static_cast<long long>(ceil(((needC+needM) + needM*(money-M0) - needC*C0) / float(needM-needC))));
+        }else if(needC > needM){
+            cHigh = min(cHigh, static_cast<long long>(floor(((needC+needM) + needM*(money-M0) - needC*C0) / float(needM-needC))));
+            cLow = max(cLow, static_cast<long long>(ceil((wait + needM*(money-M0) - needC*C0) / float(needM-needC))));
+        }else if(needC == needM){
+            if(needC*(C0+M0-money) > wait || needC*(C0+M0-money) < needC*2) return false;
+        }
     }
-    return cLow<=cHigh;
+    return cLow<=cHigh && cHigh<=money;
 }
 
 int main(){
@@ -24,12 +30,12 @@ int main(){
 
     cin>>T;
     for(int r=0; r<T; r++){
-        cin>>N>>tC>>tM;
+        cin>>N>>C0>>M0;
         for(int i=0; i<N; i++){
             cin>>friends[i][0]>>friends[i][1]>>friends[i][2];
         }
 
-        int low=0, high=tC+tM-2;
+        int low=0, high=C0+M0-2;
         while(low<high){
             int mid = (low+high)/2;
             if(valid(mid)) high = mid;
