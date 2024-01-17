0b45e31dde0955d45c352876daa1b76b31fc9dfd
cola <1020151695@qq.com>
2024-01-17 19:28:52 +0800

    update

--- silver/18-12/2.cpp
+++ silver/18-12/2.cpp
@@ -5,12 +5,12 @@ int n,arrive,spend;
 vector<vector<int>> cows;
 
 bool cmp(const vector<int>& a, const vector<int>& b){
-    return a[0] < b[0];
+    return a[0] < b[0];     // 记0为最高级别，从高到低排排列
 }
 
 struct PQcmp {
     bool operator()(const vector<int>& a, const vector<int>& b) const {
-        return a[2] < b[2];     // 级别从高到低
+        return a[2] > b[2];     // 级别从高到低
     }
 };
 
@@ -27,26 +27,24 @@ int main(){
     priority_queue<vector<int>, vector<vector<int>>, PQcmp> pq;
     int i = 0, cur = 0;
 
+    int ans = 0;
     while(i<n){
-        if(pq.empty()){
-            vector<int> top = pq.top();
-            arrive = top[0], spend = top[1];
+        if(pq.empty()){     // 等待队列为空
+            arrive = cows[i][0], spend = cows[i][1];
             cur = arrive + spend;
-            pq.pop();
-
-            while(i<n && cows[i][0] < cur){
-                pq.push(cows[i]);
-                i++;
-            }
+            i++;
         }
         else{
             vector<int> top = pq.top();
             arrive = top[0], spend = top[1];
+            ans = max(ans, cur - arrive);       // 等待时间是 cur 减去队首cow 的到达时间
             cur += spend;
             pq.pop();
         }
+        while(i<n && cows[i][0] < cur){     // 某牛吃草的时候更新pq
+            pq.push(cows[i]);
+            i++;
+        }
     }
-
-    cout<<1<<endl;
-
+    cout<<ans<<endl;
 }
