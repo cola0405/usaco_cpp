e52ddb0115791ffc9e3737ba3048b79597dc911b
cola <1020151695@qq.com>
2024-04-25 23:55:49 +0800

    O(n)dfs

--- silver/24-1/2.cpp
+++ silver/24-1/2.cpp
@@ -1,42 +1,63 @@
+// graph + O(n)dfs + leaf nodes + hidden in_deg
+
+// to achieve the first goal, the number of leaf nodes is actually the number of traversals
+// use dfs to calculate the maxinum number of potions that can be farmed at each node
+// ans is actually the potions gained from each branch of the tree
+// each branch = min(visited times, potions from sub-branch + potions generated at the node)
 #include<bits/stdc++.h>
 using namespace std;
 
-int n;
-int potions[100001];
+int n, ans=0;
+int p[100001];  // record the appearance of potions
 unordered_map<int, vector<int>> g;
-unordered_set<int> already;
-vector<vector<int>> paths(100001);
+vector<int> leaf_cnt;         // the number of leaf nodes of node i (also the number of times node i is visited)
+vector<int> generated_potion; // the number of potions generated at node i
 
-void dfs(int cur, vector<int> path){
-    if(g[cur].empty()) return;
-
-    if(already.find(cur) != already.end()) return;
-    paths[cur] = path;
-    for(auto nxt: g[cur]){
-        path.push_back(nxt);
-        dfs(nxt, path);
-        path.pop_back();
+int dfs1(int node, int pre){    
+    int cnt = 0;
+    if(g[node].size() == 1 && node != 1) cnt++;     // root is not leaf
+    for(auto nxt: g[node]){
+        if(nxt == pre) continue;
+        cnt += dfs1(nxt, node);
     }
+    return leaf_cnt[node] = cnt;
+}
+
+int dfs2(int node, int pre){
+    int cnt = 0;
+    for(auto nxt: g[node]){
+        if(nxt == pre) continue;
+        cnt += dfs2(nxt, node);
+    }
+    // the leaf_cnt[node] is actually the number of times node i is visited
+    // the number of farmed potions must less than the visited time
+    return min(leaf_cnt[node], cnt + generated_potion[node]);
 }
 
 int main(){
-    freopen("2.in", "r", stdin);
-    freopen("2.out", "w", stdout);
+    // freopen("2.in", "r", stdin);
+    // freopen("2.out", "w", stdout);
 
+    int a,b;
     cin>>n;
-    for(int i=0; i<n; i++) cin>>potions[i];
-    for(int i=0; i<n; i++){
-        int src,dst;
-        cin>>src>>dst;
-        g[src].push_back(dst);
+    leaf_cnt.resize(n+1);
+    generated_potion.resize(n+1);
+    for(int i=0; i<n; i++) cin>>p[i];
+    for(int i=0; i<n-1; i++){
+        cin>>a>>b;
+        g[a].push_back(b);
+        g[b].push_back(a);
     }
-
-    dfs();
-
-    for(int i=0; i<n; i++){
-        int dst = potions[i];
-
+    int k = 0;      // the number of leaf nodes
+    for(int i=1; i<=n; i++){
+        if(g[i].size() == 1) {      // don't need the in_deg here (hidden in_deg)
+            leaf_cnt[i]++;
+            k++;
+        }
     }
-    
-
+    k--;    // root is not leaf node
+    // the first k generated potions are available
+    for(int i=0; i<k; i++) generated_potion[p[i]]++;
+    dfs1(1,-1);        // recursively calculate the number of leaf nodes
+    cout<<dfs2(1,-1)<<endl;      // recursively calculate the number of potions
 }
